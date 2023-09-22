eb3c18d466d499b4bf9b77a70d34de347bada90a
Danial <bashirzadehdanial@gmail.com>
2023-09-22 11:09:27 +0100

    Navigation tags replaced

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
-// import useParams
+import { useParams } from 'react-router-dom';
 // import Link
 
 const HomePage = () => {
   const [data, setData] = useState(null);
-  const type = ''; // Fix me!
+  const { type} = useParams()
 
   useEffect(() => {
     async function getPetsData() {
